ee02be569aeed8f5ab386ae902628a93c5ef4229
Albert Agram <albert.agram@gmail.com>
2018-04-02 00:41:14 +0000

    show number of results and reset search

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,6 +17,10 @@ class ListContacts extends Component {
     }))
   }
 
+  clearQuery = () => {
+    this.updateQuery('')
+  }
+
   render () {
     const { query } = this.state
     const { contacts, onDeleteContact } = this.props
@@ -36,6 +40,13 @@ class ListContacts extends Component {
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now showing {showingContacts.length} of {contacts.length}</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
 
         <ol className='contact-list'>
           {showingContacts.map((contact) => (
